df0a54ac72145de8f1d87c2f7a6bd2d4fdcdf37a
Ilhan Kizildere <i.kizildere@stud.unibas.ch>
2016-11-07 11:40:40 +0100

    add @currentUsers

--- server.js
+++ server.js
@@ -9,6 +9,37 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
+
+
+function sendCurrentUsers(socket){
+	var info = clientInfo[socket.id];
+	var users = [];
+
+
+	if(typeof info === 'undifend'){
+		return;
+	}
+
+	Object.keys(clientInfo).forEach(function (socketId){
+
+var userInfo=clientInfo[socketId];
+if(info.room ===userInfo.room){
+users.push(userInfo.name);
+}
+	});
+socket.emit('message', {
+name: 'System',
+text: 'CUrrent users: ' + users.join(', '),
+timestamp: moment().valueOf()
+
+
+});
+
+}
+
+
+
+
 io.on('connection', function(socket){
 	console.log('user connected via socket.io');
 
@@ -50,11 +81,17 @@ timestamp: moment().valueOf()
 	 socket.on('message', function(message){
 		console.log('message received' ,message.text);
 
+		if(message.text === '@currentUsers'){
+			sendCurrentUsers(socket);
 
-		message.timestamp=moment().valueOf();
+		}else{
+message.timestamp=moment().valueOf();
 
 		io.to(clientInfo[socket.id].room).emit('message', message);
 
+
+		}
+		
 	});
 
 
